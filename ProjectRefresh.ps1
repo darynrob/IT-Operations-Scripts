13ba40f7aa953d3b8e2ae1305137b385f8ce1f50
darynrob <68443940+darynrob@users.noreply.github.com>
2023-02-09 12:26:01 -0800

    Create ProjectRefresh.ps1

---
+++ ProjectRefresh.ps1
@@ -0,0 +1,25 @@
+# Defining the list of computers using a text file
+$computerList = get-content -path "c:\WHEREEVERIDECIDE\computers.txt"
+
+# Here script will loop through each computer in text file
+foreach ($computer in $computerList) {
+  # Initializes the TPM module on the current computer
+  Write-Host "Initializing TPM on $computer"
+  Initialize-Tpm -ComputerName $computer
+
+  # Uninstalls the Check Point Endpoint Security if it's installed on local machine
+  Write-Host "Checking for Check Point Endpoint Security on $computer"
+  $checkPointInstalled = Get-WmiObject -Class Win32_Product | Where-Object { $_.Name -eq "Check Point Endpoint Security" }
+
+  if ($checkPointInstalled) {
+    Write-Host "Uninstalling Check Point Endpoint Security from $computer"
+    $checkPointInstalled.Uninstall()
+  }
+
+  # Enable BitLocker on the current computer
+  Write-Host "Enabling BitLocker on $computer"
+  Enable-BitLocker -MountPoint C: -EncryptionMethod Aes256 -UsedSpaceOnly -Skiphardwaretest -RecoveryPasswordProtector
+}
+
+# Output success message
+Write-Host "TPM initialization, Check Point Endpoint Security removal, and BitLocker enablement completed on all computers."
